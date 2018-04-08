5e80a2d40a6d321691b8282c3e1a2692fe0dfa29
klren0312 <kuanglong0312@sina.com>
2018-04-08 18:10:13 +0800

    食材列表组件

---
+++ client/components/FoodList/foodlist.js
@@ -0,0 +1,37 @@
+// components/FoodList/foodlist.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // 摊位名称
+    name: {
+      type: String,
+      value: '摊位名称'
+    },
+    // 食材价格
+    price: {
+      type: Number,
+      value: 10
+    },
+    // 食材点评
+    star: {
+      type: String,
+      value: "❤❤❤❤❤"
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
